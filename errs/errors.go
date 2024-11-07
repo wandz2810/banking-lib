51e3b7eec77af07316efe5c05d48054b1a727b85
quangquac <quang28102003@gmail.com>
2024-11-08 02:53:06 +0700

    added errors for authentication and authorization

--- errs/errors.go
+++ errs/errors.go
@@ -33,3 +33,17 @@ func NewValidationError(message string) *AppError {
 		Message: message,
 	}
 }
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusUnauthorized,
+		Message: message,
+	}
+}
+
+func NewAuthorizationError(message string) *AppError {
+	return &AppError{
+		Code:    http.StatusForbidden,
+		Message: message,
+	}
+}
